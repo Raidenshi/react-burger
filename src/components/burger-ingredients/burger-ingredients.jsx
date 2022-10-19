d6f786c8b7f7cb0beaa23e01ba2f16b9646558ec
Raidenshi <kiker200n@gmail.com>
2022-10-19 21:36:27 +0300

    implemented scroll, scroll on click functionality

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -2,56 +2,80 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import IngredientTabs from '../ingredient-tabs/ingredient-tabs';
 import IngredientsCard from '../ingredients-card/ingredients-card';
-import dataProps from '../../utils/types';
 import ingredientsStyles from './burger-ingredients.module.css';
 import { useSelector } from 'react-redux';
 
 function BurgerIngredients({ openModal }) {
   const data = useSelector((store) => store.ingredientsReducer.data);
+  const [current, setCurrent] = React.useState('Булки');
+
+  const refBun = React.useRef();
+  const refSauce = React.useRef();
+  const refMain = React.useRef();
+  const refCont = React.useRef();
+
+  const onScroll = () => {
+    const offset = refCont.current.offsetTop;
+    const scroll = refCont.current.scrollTop;
+    const toSauce = refSauce.current.offsetTop - offset;
+    const toMain = refMain.current.offsetTop - offset;
+    if (scroll < toSauce / 2 + 30) setCurrent('Булки');
+    if (scroll > toSauce / 2 + 30) setCurrent('Соусы');
+    if (scroll > (toMain + toSauce) / 2 + 30) setCurrent('Начинки');
+  };
+
+  const onClick = (value) => {
+    setCurrent(value);
+    if (value === 'Булки')
+      refBun.current.scrollIntoView({ behavior: 'smooth' });
+    if (value === 'Соусы')
+      refSauce.current.scrollIntoView({ behavior: 'smooth' });
+    if (value === 'Начинки')
+      refMain.current.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  const sorted = {
+    bun: [],
+    sauce: [],
+    main: [],
+  };
+
+  data.forEach((el) => {
+    if (el.type === 'bun')
+      sorted.bun.push(
+        <IngredientsCard element={el} key={el._id} openModal={openModal} />
+      );
+    if (el.type === 'sauce')
+      sorted.sauce.push(
+        <IngredientsCard element={el} key={el._id} openModal={openModal} />
+      );
+    if (el.type === 'main')
+      sorted.main.push(
+        <IngredientsCard element={el} key={el._id} openModal={openModal} />
+      );
+  });
+
   return (
     <div className={ingredientsStyles.container}>
       <h1 className="pt-10 pb-5  text text_type_main-large">Соберите бургер</h1>
-      <IngredientTabs />
-      <div className={`${ingredientsStyles.overflow} mt-10`}>
-        <h2 className="text text_type_main-medium">Булки</h2>
-        <ul className={ingredientsStyles.ingredients}>
-          {data.map((el) => {
-            if (el.type === 'bun')
-              return (
-                <IngredientsCard
-                  element={el}
-                  key={el._id}
-                  openModal={openModal}
-                />
-              );
-          })}
-        </ul>
-        <h2 className="text text_type_main-medium mt-10">Соусы</h2>
-        <ul className={ingredientsStyles.ingredients}>
-          {data.map((el) => {
-            if (el.type === 'sauce')
-              return (
-                <IngredientsCard
-                  element={el}
-                  key={el._id}
-                  openModal={openModal}
-                />
-              );
-          })}
-        </ul>
-        <h2 className="text text_type_main-medium mt-10">Начинки</h2>
-        <ul className={ingredientsStyles.ingredients}>
-          {data.map((el) => {
-            if (el.type === 'main')
-              return (
-                <IngredientsCard
-                  element={el}
-                  key={el._id}
-                  openModal={openModal}
-                />
-              );
-          })}
-        </ul>
+      <IngredientTabs current={current} onClick={onClick} />
+      <div
+        className={`${ingredientsStyles.overflow} mt-10`}
+        onScroll={onScroll}
+        ref={refCont}
+      >
+        <h2 className="text text_type_main-medium" ref={refBun}>
+          Булки
+        </h2>
+        <ul className={ingredientsStyles.ingredients}>{sorted.bun}</ul>
+        <h2 className="text text_type_main-medium mt-10" ref={refSauce}>
+          Соусы
+        </h2>
+        <ul className={ingredientsStyles.ingredients}>{sorted.sauce}</ul>
+        <h2 className="text text_type_main-medium mt-10" ref={refMain}>
+          Начинки
+        </h2>
+        <ul className={ingredientsStyles.ingredients}>{sorted.main}</ul>
       </div>
     </div>
   );
